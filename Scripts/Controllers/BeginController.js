619e8467ae0460dc62efbbc5614fdaadf4a36d2d
Dan <devdannyho@gmail.com>
2015-09-07 22:32:43 -0500

    Using $routeChangeSuccess for background img

--- Scripts/Controllers/BeginController.js
+++ Scripts/Controllers/BeginController.js
@@ -1,7 +1,7 @@
 ﻿var BeginController = function ($scope, $window, $http) {
-    $scope.init = function () {
-        $("body").css("background-image", "url('/Content/photo-1428908728789-d2de25dbd4e2.jpg')");
-    };
+    $scope.$on('$routeChangeSuccess', function () {
+        $("body").css("background-image", "url('/Content/photo-1429032021766-c6a53949594f.jpg')");
+    });
     $scope.map = { center: { latitude: 37.6, longitude: -95.665 }, zoom: 4 };
     $scope.marker = {
         id: 0,
--- Scripts/Controllers/HomePageController.js
+++ Scripts/Controllers/HomePageController.js
@@ -1,4 +1,7 @@
 ﻿var HomePageController = function ($scope, $window) {
+    $scope.$on('$routeChangeSuccess', function () {
+        $("body").css("background-image", "url('/Content/photo-1428908728789-d2de25dbd4e2.jpg')");
+    });
     $scope.toggle = function () {
         $window.location.href = '/#/Begin/';
     };
