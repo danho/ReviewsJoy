46046d9fbb8de0c71c78db176fe8c7d41b525552
Dan <devdannyho@gmail.com>
2015-09-10 22:47:49 -0500

    style landing page

--- Scripts/Controllers/HomePageController.js
+++ Scripts/Controllers/HomePageController.js
@@ -2,6 +2,12 @@
     $scope.$on('$routeChangeSuccess', function () {
         $("body").css("background-image", "url('/Content/photo-1428908728789-d2de25dbd4e2.jpg')");
     });
+    $scope.init = function () {
+        $('#intro').css({ 'height': ($(window).height()) + 'px' });
+        $(window).resize(function () {
+            $('#intro').css({ 'height': ($(window).height()) + 'px' });
+        });
+    };
     $scope.toggle = function () {
         $window.location.href = '/#/Begin/';
     };
